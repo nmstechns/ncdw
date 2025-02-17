d76ee41724ba0d421b8678766c8e1651a68de61a
nmstechns <namaste.chains@gmail.com>
2025-02-17 11:10:42 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,5 +1,3 @@
 module.exports = [
-    'http://61c5add4265d1503d35a__cr.sg;sessttl.1440:534e443745257cc3@ip.proxynet.top:823',
-    'https://61c5add4265d1503d35a__cr.sg;sessttl.1440:534e443745257cc3@ip.proxynet.top:824',
-    'socks5://61c5add4265d1503d35a__cr.sg;sessttl.1440:534e443745257cc3@ip.proxynet.top:20000',
+    'http://spsr95fn5q:E9ztq=c5xB8Qhcz0rX@dc.smartproxy.com:10000'
 ];
