31951b0c37a8e1c2bcb635d1b2b8055fb9c2dea3
nmstechns <namaste.chains@gmail.com>
2025-02-17 11:07:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    useProxy: false, // Set to true if you want to use proxies, false otherwise
+    useProxy: true, // Set to true if you want to use proxies, false otherwise
     restartDelay: 207, // Delay in seconds before restarting the process
 	minDelay: 3, // random delay for keepalive packet send
     maxDelay: 10, // random delay for keepalive packet send
